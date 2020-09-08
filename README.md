452be1026fc648c86a20d4e9ee29abd9bc8541a7
Ian Mac Moore <ianmac@blueplusred.com>
2020-09-08 00:13:01 -0500

    Legend, circle markers sized by pop

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ A map showing the prisons in MN fed by a private gDoc spreadsheet
 
 ### Resources
 - Using [Turbo87's Sidebarv2](https://github.com/Turbo87/sidebar-v2)
+- Using [ColorBrewer2](https://colorbrewer2.org/) to generate palette
 - Followed examples for gDocs integrations:
     - [Geocoding an address using script in Google Sheet](https://willgeary.github.io/data/2016/11/04/Geocoding-with-Google-Sheets.html)
     - [Use json of Google Doc spreadsheet](https://coderwall.com/p/duapqq/use-a-google-spreadsheet-as-your-json-backend)
--- static/js/logic.js
+++ static/js/logic.js
@@ -6,7 +6,42 @@ window.addEventListener('DOMContentLoaded', createMap)
 
 let map;
 let sidebar;
-let panelID = "sidebar-pane";
+let panelID = "sidebar";
+
+//Color palette for prison security levels, diverging brown to forest green
+let levelColors = ['#8c510a','#d8b365','#f6e8c3','#0077bb','#c7eae5','#5ab4ac','#01665e'];
+
+let securityLevels = {
+  "All" : levelColors[0],
+  "Min1" : levelColors[1],
+  "Min2" : levelColors[2],
+  "MinMed" : levelColors[3],
+  // "MinMed" : '#0077bb',
+  "Med" : levelColors[4],
+  "Close" : levelColors[5],
+  "Max" : levelColors[6] 
+};
+
+function getColor(stype) {
+  switch (stype) {
+    case 'All':
+      return  '#8c510a';
+    case 'Min1':
+      return '#d8b365';
+    case 'Min2':
+      return '#f6e8c3';
+    case 'MinMed':
+      return '#0077bb';
+    case 'Med':
+      return '#c7eae5';
+    case 'Close':
+      return '#5ab4ac';
+    case 'Max':
+      return '#01665e';
+    default:
+      return '#111111';
+  }
+}
 
 function createMap(prisons) {
   console.log("Creating map")
@@ -62,119 +97,49 @@ function createMap(prisons) {
   // Change the position of the Zoom Control to a newly created placeholder.
   map.zoomControl.setPosition('topright');
   // Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
-  L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
-  }).addTo(map);
+  // L.control.layers(baseMaps, overlayMaps, {
+  //   collapsed: false
+  // }).addTo(map);
+
+
+  var legend = L.control({position: 'bottomright'});
+  // console.log("I am not legend");
+  legend.onAdd = function (map) {
+    // console.log("I am legend");
+    var div = L.DomUtil.create('div', 'info-legend');
+    labels = ['<strong>Security Level</strong>'],
+    categories = ["All","Min1","Min2","MinMed","Med","Close","Max"];
+
+    for (var i = 0; i < categories.length; i++) {
+      // console.log("legend color: ",getColor(categories[i]));
+      div.innerHTML += labels.push(
+          '<i class="fas fa-circle" style="color:' + getColor(categories[i]) + '"></i> ' +
+      (categories[i] ? categories[i] : '+'));
+    //   div.innerHTML += labels.push(
+    //     '<i class="fas fa-circle" style="background:' + "#111111" + '"></i> ' +
+    // (categories[i] ? categories[i] : '+'));
+      div.innerHTML = labels.join('<br>');
+    };
+    return div;
+  }
+  legend.addTo(map);
+  // // Handmade legend
+  // let legendArea = d3.select("map").append("g")
+  //   .attr("transform", "translate(10,-90)");
+  // let legendNames = ["All","Min1","Min2","MinMed","Med","Close","Max"];
+  // let legendColors = levelColors;
+  // let yVal = 100;
+  
+  // for (let i = 0; i < legendNames.length; i++) {
+  //   let xCircle = 50;
+  //   let xText = 70;
+
+  //   yVal = yVal + 30;
+
+  //   legendArea.append("circle").attr("cx", xCircle).attr("cy", yVal).attr("r", 10).style("fill", legendColors[i]).style("stroke", "black");
+  //   legendArea.append("text").attr("x", xText).attr("y", yVal).text(legendNames[i]).style("font-size", "15px").attr("alignment-baseline", "middle");
+  // };
 }
-
-// Using CSV //////////////////////////////////////////////////////////
-// function addPoints(data) {
-//   console.log("Inside addPoints")
-//   console.log("Passed data:")
-//   console.log(data)
-//   data = data.data;
-//   let pointGroupLayer = L.layerGroup().addTo(map);
-
-//   // Choose marker type. Options are:
-//   // (these are case-sensitive, defaults to marker!)
-//   // marker: standard point with an icon
-//   // circleMarker: a circle with a radius set in pixels
-//   // circle: a circle with a radius set in meters
-//   let markerType = "marker";
-
-//   // Marker radius
-//   // Wil be in pixels for circleMarker, metres for circle
-//   // Ignore for point
-//   let markerRadius = 100;
-
-//   for (let row = 0; row < data.length; row++) {
-//     let marker;
-//     if (markerType == "circleMarker") {
-//       marker = L.circleMarker([data[row].lat, data[row].lng], {
-//         radius: markerRadius,
-//       });
-//     } else if (markerType == "circle") {
-//       marker = L.circle([data[row].lat, data[row].lng], {
-//         radius: markerRadius,
-//       });
-//     } else {
-//       marker = L.marker([data[row].lat, data[row].lng]);
-//     }
-//     marker.addTo(pointGroupLayer);
-
-//     // UNCOMMENT THIS LINE TO USE POPUPS
-//     marker.bindPopup('<h2>' + data[row].facility + "</h2>Address: " + data[row].address);
-
-//     // COMMENT THE NEXT GROUP OF LINES TO DISABLE SIDEBAR FOR THE MARKERS
-//     marker.feature = {
-//       properties: {
-//         name: data[row].facility,
-//         address: data[row].address,
-//       },
-//     };
-//     marker.on({
-//       click: function (e) {
-//         L.DomEvent.stopPropagation(e);
-//         // document.getElementById("sidebar-title").innerHTML =
-//         //   e.target.feature.properties.facility;
-//         document.getElementById("sidebar-content").innerHTML =
-//           e.target.feature.properties.name;
-//         sidebar.open(panelID);
-//       },
-//     });
-//     // COMMENT UNTIL HERE TO DISABLE SIDEBAR FOR THE MARKERS
-//     console.log("Facility: ", data[row].facility)
-//     // Fill sidebar
-//     d3.select('#home')
-//       .append('p')
-//       .text(data[row].facility)
-
-//     // AwesomeMarkers is used to create fancier icons
-//     let icon = L.AwesomeMarkers.icon({
-//       icon: "info-circle",
-//       iconColor: "white",
-//       markerColor: data[row].color,
-//       prefix: "fa",
-//       extraClasses: "fa-rotate-0",
-//     });
-//     if (!markerType.includes("circle")) {
-//       marker.setIcon(icon);
-//     }
-//   }
-// }
-
-// function getData(){
-//   const express = require('express');
-//   const request = require('request');
-
-//   const app = express();
-
-//   app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     next();
-//   });
-
-//   app.get('/e', (req, res) => {
-//     request(
-//       { url: pointsURL },
-//       (error, response, body) => {
-//         if (error || response.statusCode !== 200) {
-//           return res.status(500).json({ type: 'error', message: err.message });
-//         }
-
-//         Papa.parse(pointsURL, {
-//           download: true,
-//           header: true,
-//           complete: addPoints,
-//         });
-//       }
-//     )
-//   });
-
-//   const PORT = process.env.PORT || 3000;
-//   app.listen(PORT, () => console.log(`listening on ${PORT}`));
-// }
-//END USING CSV
 
 
 //Using JSON link
@@ -191,7 +156,7 @@ function addPoints() {
   // marker: standard point with an icon
   // circleMarker: a circle with a radius set in pixels
   // circle: a circle with a radius set in meters
-  let markerType = "marker";
+  let markerType = "circleMarker";
 
   // Marker radius
   // Wil be in pixels for circleMarker, metres for circle
@@ -201,17 +166,39 @@ function addPoints() {
   d3.json(pointsURLJSON, function (data) {
     console.log("JSON", data);
     // console.log("Facility: ", data.feed.entry[0].gsx$facility.$t)
+    
     for (let row = 0; row < data.feed.entry.length; row++) {
-      console.log("row: ", row)
-      console.log("Facility: ", data.feed.entry[row].gsx$facility.$t)
       let marker;
+      // let secColor;
+      console.log("Facility data: ", data.feed.entry[row].gsx$facility.$t)
+      
       if (markerType == "circleMarker") {
+        console.log("Custody Level color: ",securityLevels[data.feed.entry[row].gsx$custodylevel.$t]);
+        console.log("Color getColor: ", getColor(data.feed.entry[row].gsx$custodylevel.$t))
+        secColor = securityLevels[data.feed.entry[row].gsx$custodylevel.$t];
         marker = L.circleMarker([data.feed.entry[row].gsx$lat.$t, data.feed.entry[row].gsx$lng.$t], {
-          radius: markerRadius,
+          // radius: markerRadius,
+          radius: parseInt(data.feed.entry[row].gsx$population.$t)/100,
+          // color: `'${securityLevels[data.feed.entry[row].gsx$custodylevel.$t]}'`,
+          // color: `'${secColor}'`,
+          // color: '#0077bb',
+          color: getColor(data.feed.entry[row].gsx$custodylevel.$t),
+          opacity: 1,
+          weight: 4
         });
+        // marker.setStyle({ 
+        //   color: `"${levelColors[4]}"`
+        // });
       } else if (markerType == "circle") {
+        console.log("Custody Level: ",data.feed.entry[row].gsx$custodylevel.$t);
+        console.log("Custody Level color: ",securityLevels[data.feed.entry[row].gsx$custodylevel.$t]);
         marker = L.circle([data.feed.entry[row].gsx$lat.$t, data.feed.entry[row].gsx$lng.$t], {
-          radius: markerRadius,
+          // radius: markerRadius,
+          radius: parseInt(data.feed.entry[row].gsx$population.$t)*10,
+          // color: `'${securityLevels[data.feed.entry[row].gsx$custodylevel.$t]}'`,
+          // color: '#0077bb',
+          opacity: 1,
+          weight: 10
         });
       } else {
         marker = L.marker([data.feed.entry[row].gsx$lat.$t, data.feed.entry[row].gsx$lng.$t]);
@@ -219,43 +206,47 @@ function addPoints() {
       marker.addTo(pointGroupLayer);
 
       // UNCOMMENT THIS LINE TO USE POPUPS
-      marker.bindPopup('<h2>' + data.feed.entry[row].gsx$facility.$t + "</h2>Address: " + data.feed.entry[row].gsx$address.$t);
+      marker.bindPopup(`<h4>${data.feed.entry[row].gsx$facility.$t}</h4>Address:  
+        ${data.feed.entry[row].gsx$address.$t}`);
 
-      // COMMENT THE NEXT GROUP OF LINES TO DISABLE SIDEBAR FOR THE MARKERS
-      marker.feature = {
-        properties: {
-          name: data.feed.entry[row].gsx$facility.$t,
-          address: data.feed.entry[row].gsx$address.$t,
-        },
-      };
-      marker.on({
-        click: function (e) {
-          L.DomEvent.stopPropagation(e);
-          // document.getElementById("sidebar-title").innerHTML =
-          //   e.target.feature.properties.facility;
-          document.getElementById("sidebar-content").innerHTML =
-            e.target.feature.properties.name;
-          sidebar.open(panelID);
-        },
-      });
-      // COMMENT UNTIL HERE TO DISABLE SIDEBAR FOR THE MARKERS
+      // // COMMENT THE NEXT GROUP OF LINES TO DISABLE SIDEBAR FOR THE MARKERS
+      // marker.feature = {
+      //   properties: {
+      //     name: data.feed.entry[row].gsx$facility.$t,
+      //     address: data.feed.entry[row].gsx$address.$t,
+      //   },
+      // };
+      // marker.on({
+      //   click: function (e) {
+      //     L.DomEvent.stopPropagation(e);
+      //     // document.getElementById("sidebar-title").innerHTML =
+      //     //   e.target.feature.properties.facility;
+      //     document.getElementById("sidebar").innerHTML =
+      //       e.target.feature.properties.name;
+      //     sidebar.open(panelID);
+      //   },
+      // });
+      // // COMMENT UNTIL HERE TO DISABLE SIDEBAR FOR THE MARKERS
       
-      // Fill sidebar
-      d3.select('#home')
-        .append('p')
-        .text(data.feed.entry[row].gsx$facility.$t)
+      // Fill sidebar 
+      const ul = document.getElementById('list');
+      const newItem = document.createElement('li');
+
+      newItem.appendChild(document.createTextNode(data.feed.entry[row].gsx$facility.$t));
+
+      ul.appendChild(newItem).classList.add("nav-item");
 
       // // AwesomeMarkers is used to create fancier icons
-      // let icon = L.AwesomeMarkers.icon({
-      //   icon: "info-circle",
-      //   iconColor: "white",
-      //   markerColor: data[row].color,
-      //   prefix: "fa",
-      //   extraClasses: "fa-rotate-0",
-      // });
-      // if (!markerType.includes("circle")) {
-      //   marker.setIcon(icon);
-      // }
+      let icon = L.AwesomeMarkers.icon({
+        icon: "asterisk",
+        iconColor: "white",
+        markerColor: "darkred",
+        prefix: "fa",
+        extraClasses: "fa-rotate-0",
+      });
+      if (!markerType.includes("circle")) {
+        marker.setIcon(icon);
+      }
     }
   });
 }
